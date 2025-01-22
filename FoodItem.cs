520b6e6a8d884a607d1eb10f5fba0b977b7c5a98
Davis Larson <davislarson01@gmail.com>
2025-01-22 15:44:56 -0700

    make the constructor public

--- FoodItem.cs
+++ FoodItem.cs
@@ -4,17 +4,17 @@ namespace Mission3FoodBank;
 
 public class FoodItem
 {
+    // Constructor for the foodItem
     public string Name {get; set;}
     public string Category {get; set;}
     public int Quantity {get; set;}
     public DateTime ExpiryDate {get; set;}
 
-    FoodItem(string name, string category, int quantity, DateTime expiryDate)
+    public FoodItem(string name, string category, int quantity, DateTime expiryDate)
     {
         Name = name;
         Category = category;
         Quantity = quantity;
         ExpiryDate = expiryDate;
     }
-    
 }
